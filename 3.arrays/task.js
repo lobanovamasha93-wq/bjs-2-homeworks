e17434e9c59c82666c0286dee5193f4d496f6790
Maria Lobanova <marusa.lobanova2015@yandex.ru>
2026-05-26 16:21:03 +0000

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filtered = users.filter((user) => user.gender === gender);
+  if (filtered.length === 0) {
+    return 0;
+  }
+  return filtered.map((user) => user.age).reduce((sum, age) => sum + age, 0) / filtered.length;
 }
